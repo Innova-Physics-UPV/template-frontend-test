0aede64aeed535a94f06ae890ded2ce660d0faea
bernatt04 <127421514+bernatt04@users.noreply.github.com>
2023-12-14 13:44:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.onload = function() {
-  //el contenido de esta función se ejecuta automáticamente al ser iniciada la página.
-  //puedes borrarla si no la necesitas
-};
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector(".myHeader"); // Cambio aquí
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+
+  
+  var prevScrollpos = window.pageYOffset;
+
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-100px"; // Change the value to the height of your header to completely hide it
+    }
+    prevScrollpos = currentScrollPos;
+  }
+  
+
+  window.onscroll = function() {
+    stickyHeader();
+  };
+
+  menuToggle.addEventListener('click', function() {
+    this.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+});
